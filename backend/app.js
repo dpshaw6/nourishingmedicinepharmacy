399b564534bfbf00f0a5805eda7f4d3f83310de1
Daniel Shaw <dpshaw@gmail.com>
2023-11-22 17:02:28 -0800

    Initial Commit

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+// Define a simple route
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
